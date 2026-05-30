a67afeb947a642fffca6b4bd9ffa58503e2e8388
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-05-30 14:41:38 +0200

    Updated the script

--- chat-app/back/server.js
+++ chat-app/back/server.js
@@ -1,16 +1,16 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from 'ws' // ← add WebSocket here
 
 const wss = new WebSocketServer({ port: 3000 })
 
 console.log('Socket is running')
 
-const data = []
 wss.on('connection', (socket) => {
   console.log('Connected client')
   socket.on('message', (message) => {
     const messageString = message.toString()
     for (const client of wss.clients) {
       if (client.readyState === WebSocket.OPEN) {
+        // ← now this works
         client.send(messageString)
         console.log(JSON.parse(messageString))
       }
